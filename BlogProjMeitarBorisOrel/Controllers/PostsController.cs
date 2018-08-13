a8f3502c7adb8e44191ae4bc80ba702901f28a26
boris <borisbinaev@gmail.com>
2018-08-13 16:01:45 +0300

    bind

--- BlogProjMeitarBorisOrel/Controllers/PostsController.cs
+++ BlogProjMeitarBorisOrel/Controllers/PostsController.cs
@@ -76,7 +76,7 @@ namespace BlogProjMeitarBorisOrel.Controllers
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("ID,UserID,PublishedDate,Title,Author_Name,Text,UrlImage")] Post post)
+        public async Task<IActionResult> Create([Bind("ID,UserID,PublishedDate,Title,Author_Name,Text,UrlImage,NumOfLikes")] Post post)
         {
             if (ModelState.IsValid)
             {
@@ -110,7 +110,7 @@ namespace BlogProjMeitarBorisOrel.Controllers
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("ID,UserID,PublishedDate,Title,Author_Name,Text,UrlImage")] Post post)
+        public async Task<IActionResult> Edit(int id, [Bind("ID,UserID,PublishedDate,Title,Author_Name,Text,UrlImage,NumOfLikes")] Post post)
         {
             if (id != post.ID)
             {
