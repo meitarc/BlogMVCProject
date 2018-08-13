c734050749d028eb407727f3e4a2dd95bb697f18
Meitar <meitarc@gmail.com>
2018-08-13 16:28:45 +0300

    add group by to post

--- BlogProjMeitarBorisOrel/Controllers/PostsController.cs
+++ BlogProjMeitarBorisOrel/Controllers/PostsController.cs
@@ -21,8 +21,31 @@ namespace BlogProjMeitarBorisOrel.Controllers
         }
 
         // GET: Posts
-        public async Task<IActionResult> Index(string searchString, string searchString2, string searchString3)
+        public async Task<IActionResult> Index(string searchString, string searchString2, string searchString3, string gBy, string jBy)
         {
+
+            if (gBy == "Aname")
+            {
+                var userNamesByID =
+                   from u in _context.Post
+                   group u by u.Author_Name into g
+                   select new { Author_Name = g.Key, count = g.Count(), g.First().Title };
+                var group = new List<Post>();
+                foreach (var t in userNamesByID)
+                {
+                    group.Add(new Post()
+                    {
+                        Author_Name = t.Author_Name,
+                        Counter = t.count,
+                        Title = t.Title
+
+                    });
+                }
+            
+                return View(group);
+            }
+            else { 
+
             var posts = from s in _context.Post
                         select s;
             if (!String.IsNullOrEmpty(searchString))
@@ -41,8 +64,9 @@ namespace BlogProjMeitarBorisOrel.Controllers
             }
 
             return View(posts.ToList());
-            //var applicationDbContext = _context.Post.Include(p => p.User);
-            //return View(await applicationDbContext.ToListAsync());
+                //var applicationDbContext = _context.Post.Include(p => p.User);
+                //return View(await applicationDbContext.ToListAsync());
+            }
         }
 
         // GET: Posts/Details/5
