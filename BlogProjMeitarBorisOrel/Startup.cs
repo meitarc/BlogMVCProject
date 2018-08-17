15e8bad0ca6df5aeb7596ea0b5ba4b8e112706a1
boris <borisbinaev@gmail.com>
2018-08-17 14:15:06 +0300

    admin +machine learning

--- BlogProjMeitarBorisOrel/Startup.cs
+++ BlogProjMeitarBorisOrel/Startup.cs
@@ -12,6 +12,7 @@ using BlogProjMeitarBorisOrel.Data;
 using BlogProjMeitarBorisOrel.Models;
 using BlogProjMeitarBorisOrel.Services;
 
+
 namespace BlogProjMeitarBorisOrel
 {
     public class Startup 
@@ -35,12 +36,32 @@ namespace BlogProjMeitarBorisOrel
 
             // Add application services.
             services.AddTransient<IEmailSender, EmailSender>();
-
+        
             services.AddMvc();
         }
 
+        private async Task CreateUserRoles(IServiceProvider serviceProvider)
+        {
+            var RoleManager = serviceProvider.GetRequiredService<RoleManager<IdentityRole>>();
+            var UserManager = serviceProvider.GetRequiredService<UserManager<ApplicationUser>>();
+
+
+            IdentityResult roleResult;
+            //Adding Addmin Role  
+            var roleCheck = await RoleManager.RoleExistsAsync("Admin");
+            if (!roleCheck)
+            {
+                //create the roles and seed them to the database  
+                roleResult = await RoleManager.CreateAsync(new IdentityRole("Admin"));
+            }
+            //Assign Admin role to the main User here we have given our newly loregistered login id for Admin management  
+            ApplicationUser user = await UserManager.FindByEmailAsync("borisbinaev@gmail.com");
+            var User = new ApplicationUser();
+            await UserManager.AddToRoleAsync(user, "Admin");
+
+        }
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env,IServiceProvider services)
         {
             if (env.IsDevelopment())
             {
@@ -63,6 +84,7 @@ namespace BlogProjMeitarBorisOrel
                     name: "default",
                     template: "{controller=Posts}/{action=Index}/{id?}");
             });
+            CreateUserRoles(services).Wait();
         }
     }
 }
