23f8dceaa7d795c07614d0e4760610bd81ea2968
Meitar <meitarc@gmail.com>
2018-08-19 15:01:33 +0300

    add a lot of things

--- BlogProjMeitarBorisOrel/Controllers/HomeController.cs
+++ BlogProjMeitarBorisOrel/Controllers/HomeController.cs
@@ -6,6 +6,14 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using BlogProjMeitarBorisOrel.Models;
 using BlogProjMeitarBorisOrel.Data;
+using Microsoft.AspNetCore.Authorization;
+
+
+using System.Web;
+
+
+
+using Newtonsoft.Json;
 
 namespace BlogProjMeitarBorisOrel.Controllers
 {
@@ -37,4 +45,139 @@ namespace BlogProjMeitarBorisOrel.Controllers
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
     }
+
+    //public class HomeController : Controller
+
+    //{
+
+    //    //
+
+    //    // GET: /Home/
+
+
+
+
+    //    public ActionResult Index()
+
+    //    {
+
+    //        return View();
+
+    //    }
+
+
+
+
+    //    private Uri RediredtUri
+
+    //    {
+
+    //        get
+
+    //        {
+
+    //            var uriBuilder = new UriBuilder();
+
+    //            uriBuilder.Query = null;
+
+    //            uriBuilder.Fragment = null;
+
+    //            uriBuilder.Path = Url.Action("FacebookCallback");
+
+    //            return uriBuilder.Uri;
+
+    //        }
+
+    //    }
+
+
+
+
+    //    [AllowAnonymous]
+
+    //    public ActionResult Facebook()
+
+    //    {
+
+    //        var fb = new FacebookClient();
+
+    //        var loginUrl = fb.GetLoginUrl(new
+
+    //        {
+
+
+
+
+    //            client_id = "249043415725802",
+
+    //            client_secret = "6d75928bcda675afbd25a9af20efbe5f",
+
+    //            redirect_uri = RediredtUri.AbsoluteUri,
+
+    //            response_type = "code",
+
+    //            scope = "email"
+
+
+
+    //        });
+
+    //        return Redirect(loginUrl.AbsoluteUri);
+
+    //    }
+
+
+
+
+    //    public ActionResult FacebookCallback(string code)
+
+    //    {
+
+    //        var fb = new FacebookClient();
+
+    //        dynamic result = fb.Post("oauth/access_token", new
+
+    //        {
+
+    //            client_id = "Your App ID",
+
+    //            client_secret = "Your App Secret key",
+
+    //            redirect_uri = RediredtUri.AbsoluteUri,
+
+    //            code = code
+
+
+
+
+    //        });
+
+    //        var accessToken = result.access_token;
+
+    //        //Session["AccessToken"] = accessToken;
+
+    //        fb.AccessToken = accessToken;
+
+    //        dynamic me = fb.Get("me?fields=link,first_name,currency,last_name,email,gender,locale,timezone,verified,picture,age_range");
+
+    //        string email = me.email;
+
+    //        TempData["email"] = me.email;
+
+    //        TempData["first_name"] = me.first_name;
+
+    //        TempData["lastname"] = me.last_name;
+
+    //        TempData["picture"] = me.picture.data.url;
+
+    //        //FormsAuthentication.SetAuthCookie(email, false);
+
+    //        return RedirectToAction("Index", "Home");
+
+    //    }
+
+
+
+
+    //}
 }
